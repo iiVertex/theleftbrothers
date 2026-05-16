23b33cfab8f8eeba1ae76bb16d880f6b76b7c817
Ammar Shoeb <as2506214@qu.edu.qa>
2026-05-16 19:27:28 +0300

    updated App.js minorly
    changed the pre-existing comment

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import AppNavigator from './src/navigation/AppNavigator';
 import { DataProvider } from './src/context/DataContext';
 import { AuthProvider } from './src/context/AuthContext';
 
-// hi
+// hi, ammar is trans !
 
 export default function App() {
   return (
